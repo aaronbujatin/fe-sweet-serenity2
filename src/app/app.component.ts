ea92a8cd047f67d8ffa79d68a089033648823736
Aaron Bujatin <aaronbujatin@gmail.com>
2023-10-27 12:15:54 +0800

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { initFlowbite } from 'flowbite';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'fe-sweet-serenity2';
+  title = 'fe-sweet-serenity';
+
+  ngOnInit(): void {
+    initFlowbite();
+  }
+
 }
---
+++ src/app/components/contact/contact.component.ts
@@ -0,0 +1,49 @@
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { User } from 'src/app/model/user';
+import { UserService } from 'src/app/service/user.service';
+import Swal from 'sweetalert2';
+
+@Component({
+  selector: 'app-contact',
+  templateUrl: './contact.component.html',
+  styleUrls: ['./contact.component.css']
+})
+export class ContactComponent {
+
+  constructor(private userService: UserService, private myRoute: Router) { }
+
+  user: User = new User();
+
+
+
+  onSubmit() {
+
+    this.userService.sendInquiry(this.user).subscribe(
+      (response) => {
+        this.successAlert()
+      }, (error) => {
+        console.log(error);
+      }
+    )
+  }
+
+  successAlert() {
+    Swal.fire({
+      text: "Your reservation was successful. You will now redirect to our homepage",
+      icon: "success",
+      confirmButtonText: "OK",
+      showCloseButton: true,
+      confirmButtonColor: '#E8AAAD',
+    }).then((result) => {
+      if (result.isConfirmed) {
+        this.myRoute.navigate([""]);
+      }
+    });
+  }
+
+  isValidGmail(email: string): boolean {
+    return email?.toLowerCase().endsWith('@gmail.com');
+
+  }
+}
